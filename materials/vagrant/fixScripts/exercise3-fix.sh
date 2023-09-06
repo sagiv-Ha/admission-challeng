c327619da6810f1d09e961cd277133036804ae26
sagiv-Ha <harir.sagiv@gmail.com>
2023-09-06 14:03:12 +0300

    fixscript

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise3 here
+sudo sed -i '/# vim: syntax=apache ts=4 sw=4 sts=4 sr noet/,${/<Location "\/">/,/<\/Location>/d}' /etc/apache2/sites-available/000-default.conf
+
+sudo systemctl restart apache2.service
